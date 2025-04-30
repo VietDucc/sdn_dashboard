35e233d9b3d84b6f54eddba22498b89be6bd077f
VietDucc <21521959@gm.uit.edu.vn>
2025-04-30 23:19:43 +0700

    Dashboard

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,js,jsx,ts,tsx}", // Điều chỉnh theo cấu trúc dự án của bạn
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
